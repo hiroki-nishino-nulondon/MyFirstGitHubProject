ba5ea4287a2d6ee993ba405d547084663f1a89b0
Hiroki Nishino <h.nishion@nulondon.ac.uk>
2025-10-29 15:17:12 +0000

    just a small edit made to my_new_file.py (locally)

--- my_new_file.py
+++ my_new_file.py
@@ -1,2 +1,5 @@
 print("Is this a new file someone added to the repository?")
 print("Or did another collaborator push it?")
+
+print("I added this line on my laptop to trigger a merge confilict!")
+
