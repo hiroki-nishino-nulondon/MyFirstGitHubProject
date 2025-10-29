c25433d1449e9fd019d1b1f876949b974cb30fe3
Hiroki Nishino <h.nishion@nulondon.ac.uk>
2025-10-29 17:21:47 +0000

    try pushing as a collaborator

--- my_new_file.py
+++ my_new_file.py
@@ -4,3 +4,5 @@ print("Or did another collaborator push it?")
 print("I added this line on my laptop to trigger a merge confilict!")
 print("I added this line on GitHub to trigger a merge conflict between your local branch and the remote.")
 
+print("Hello, I am your collaborator!")
+
