c7b83d80739b3b5b5bf326df159f9a345044a541
hiroki-nishino-nulondon <h.nishino@northeastern.edu>
2025-10-29 14:43:15 +0000

    Update my_new_file.py
    let's cause a merge conflict

--- my_new_file.py
+++ my_new_file.py
@@ -1,2 +1,4 @@
 print("Is this a new file someone added to the repository?")
 print("Or did another collaborator push it?")
+
+print("I added this line on GitHub to trigger a merge conflict between your local branch and the remote.")
